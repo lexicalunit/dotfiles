e8ccd182ba7c71412c610c4039161ccb761d997c
lexicalunit <lexicalunit@lexicalunit.com>
2017-09-27 13:59:49 -0500

    Auto hide tree view in atom.

--- .atom/init.coffee
+++
@@ -1,6 +0,0 @@
-# Markdown files should always be soft wrapped.
-path = require 'path'
-atom.workspace.observeTextEditors (editor) ->
-  editorView = atom.views.getView(editor)
-  if path.extname(String(editor.getPath())) is '.md'
-    editor.setSoftWrapped(true)
---
+++ .atom/init.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+// Markdown files should always be soft wrapped.
+atom.workspace.observeTextEditors((editor) => {
+  editorView = atom.views.getView(editor)
+  if (path.extname(String(editor.getPath())) === '.md') {
+    editor.setSoftWrapped(true)
+  }
+})
+
+// Hide tree view by default on editor startup.
+const workspaceView = atom.views.getView(atom.workspace)
+workspaceView.classList.add('loading')
+atom.packages.onDidActivateInitialPackages(() => {
+  const workspaceView = atom.views.getView(atom.workspace)
+  atom.commands.dispatch(workspaceView, 'tree-view:toggle')
+  workspaceView.classList.remove('loading')
+})

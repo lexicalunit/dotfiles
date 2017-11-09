bad1427e1922a38188f5e795ec667e277c499d27
lexicalunit <lexicalunit@lexicalunit.com>
2017-11-09 15:24:09 -0600

    Updates atom settings.

--- .atom/init.js
+++ .atom/init.js
@@ -1,18 +1,19 @@
-const path = require('path')
+// const path = require('path')
 
 // Markdown files should always be soft wrapped.
-atom.workspace.observeTextEditors((editor) => {
-  editorView = atom.views.getView(editor)
-  if (path.extname(String(editor.getPath())) === '.md') {
-    editor.setSoftWrapped(true)
-  }
-})
+// atom.workspace.observeTextEditors((editor) => {
+//   editorView = atom.views.getView(editor)
+//   if (path.extname(String(editor.getPath())) === '.md') {
+//     editor.setSoftWrapped(true)
+//   }
+// })
 
 // Hide tree view by default on editor startup.
-const workspaceView = atom.views.getView(atom.workspace)
-workspaceView.classList.add('loading')
-atom.packages.onDidActivateInitialPackages(() => {
-  const workspaceView = atom.views.getView(atom.workspace)
-  atom.commands.dispatch(workspaceView, 'tree-view:toggle')
-  workspaceView.classList.remove('loading')
-})
+// See: https://discuss.atom.io/t/hide-treeview-by-default/7718/38
+// const workspaceView = atom.views.getView(atom.workspace)
+// workspaceView.classList.add('loading')
+// atom.packages.onDidActivateInitialPackages(() => {
+//   const workspaceView = atom.views.getView(atom.workspace)
+//   atom.commands.dispatch(workspaceView, 'tree-view:toggle')
+//   workspaceView.classList.remove('loading')
+// })

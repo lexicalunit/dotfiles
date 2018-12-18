d6f3715a0805bfbe9645bd3eb3c76d4672c52042
lexicalunit <lexicalunit@lexicalunit.com>
2018-12-18 11:57:56 -0800

    Updates more code and atom settings.

--- .atom/init.js
+++ .atom/init.js
@@ -1,19 +1,5 @@
-// const path = require('path')
-
-// Markdown files should always be soft wrapped.
-// atom.workspace.observeTextEditors((editor) => {
-//   editorView = atom.views.getView(editor)
-//   if (path.extname(String(editor.getPath())) === '.md') {
-//     editor.setSoftWrapped(true)
-//   }
-// })
-
-// Hide tree view by default on editor startup.
-// See: https://discuss.atom.io/t/hide-treeview-by-default/7718/38
-// const workspaceView = atom.views.getView(atom.workspace)
-// workspaceView.classList.add('loading')
-// atom.packages.onDidActivateInitialPackages(() => {
-//   const workspaceView = atom.views.getView(atom.workspace)
-//   atom.commands.dispatch(workspaceView, 'tree-view:toggle')
-//   workspaceView.classList.remove('loading')
-// })
+// startup immediately with the atom-notes application
+atom.packages.onDidActivateInitialPackages(() => {
+  const workspace = atom.views.getView(atom.workspace)
+  atom.commands.dispatch(workspace, 'atom-notes:toggle')
+})
